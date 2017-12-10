3273ac13cf0687493e07d542f2df2eed0bbc572e
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2017-12-10 17:37:02 +0000

    Initial Commit
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

---
+++ mpv-libretro.c
@@ -0,0 +1,26 @@
+#include "libretro.h"
+
+unsigned retro_api_version(void)
+{
+   return RETRO_API_VERSION;
+}
+
+void retro_init(void)
+{
+	return;
+}
+
+void retro_set_controller_port_device(unsigned port, unsigned device)
+{
+   log_cb(RETRO_LOG_INFO, "Plugging device %u into port %u.\n", device, port);
+}
+
+void retro_get_system_info(struct retro_system_info *info)
+{
+   memset(info, 0, sizeof(*info));
+   info->library_name     = "mpv";
+   info->library_version  = "v1";
+   info->need_fullpath    = true;
+   info->valid_extensions = "mkv|avi|f4v|f4f|3gp|ogm|flv|mp4|mp3|flac|ogg|m4a|webm|3g2|mov|wmv|mpg|mpeg|vob|asf|divx|m2p|m2ts|ps|ts|mxf|wma|wav";
+}
+
